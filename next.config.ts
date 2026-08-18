d2835d97da6b63bef238d6760f31d97d2c098ef5
Naha1981 <naha@2ndlife.ai>
2026-08-18 17:31:14 +0200

    fix: remove standalone output for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   outputFileTracingRoot: path.join(__dirname),
   typescript: {
     ignoreBuildErrors: true,
